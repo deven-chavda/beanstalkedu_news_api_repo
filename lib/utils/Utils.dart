30a1c987f52148c57590f4228449e7db4b014611
Deven <mymartplac@gmail.com>
2022-12-14 04:05:09 +0530

    Utils.dart | file for useful code to written in it. added routeConst for route path, updated showSnackBar with other params, validateQueryText for validation of searched query for api, getPageSizeList return list with number for max items can be present in single page

--- lib/utils/Utils.dart
+++ lib/utils/Utils.dart
@@ -1,7 +1,69 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class Utils {
+  // This class is for writing re-usable functions and etc.
+  var routeConst = {'home': '/'};
+
   showSnackBar(String title, String message) {
-    Get.snackbar(title, message);
+    Get.snackbar(
+      title,
+      message,
+      backgroundColor:
+          title.toLowerCase().contains('error') ? Colors.red : Colors.grey,
+      snackPosition: SnackPosition.BOTTOM,
+      borderColor:
+          title.toLowerCase().contains('error') ? Colors.red : Colors.grey,
+      borderRadius: 20,
+      borderWidth: 2,
+      margin: const EdgeInsets.all(15),
+      colorText: Colors.white,
+      duration: const Duration(seconds: 4),
+      isDismissible: true,
+      dismissDirection: DismissDirection.horizontal,
+      forwardAnimationCurve: Curves.easeOutBack,
+    );
   }
+
+  validateQueryText(String query) {
+    if (query.isEmpty) {
+      return 'Please enter some text';
+    } else if (query.length < 3) {
+      return 'Please enter minimum 3 chars';
+    }
+    return null;
+  }
+
+  List<String> getPageSizeList() {
+    return ['5', '10', '15'];
+  }
+
+// getAndStoreArticles(String query) async {
+//   Response res = await NewsApiProvider().getEveryThingAPI(query);
+//   Map<String, dynamic> body = json.decode(json.encode(res.body));
+//   debugPrint('BODY : ${body['status']}');
+//   debugPrint('BODY totalResults : ${body['totalResults']}');
+//   if (body['status'] == 'error' || body['totalResults'] <= 0) {
+//     debugPrint('No Article Available');
+//     return;
+//   } else if (body['status'] == 'ok') {
+//     debugPrint('BODY : ${body['articles'][0]['source']['id']}');
+//     var box = objectbox.store.box<ArticlesEntity>();
+//     for (var article in body['articles']) {
+//       var articleEntity = ArticlesEntity(
+//           article['author'] ?? '',
+//           article['title'] ?? '',
+//           article['content'] ?? '',
+//           article['description'] ?? '',
+//           article['publishedAt'] ?? '',
+//           article['url'] ?? '',
+//           article['urlToImage'] ?? '');
+//       articleEntity.articleSource.target = ArticleSourceEntity(
+//           article['source']['id'] ?? '', article['source']['name'] ?? '');
+//       box.put(articleEntity);
+//     }
+//   }
+// }
 }
