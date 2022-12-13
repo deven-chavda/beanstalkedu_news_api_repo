9f5c592b80ea2af5f4d74cf8b0ffc4f260efc537
Deven <mymartplac@gmail.com>
2022-12-14 05:03:06 +0530

    main.dart | entry level file having global keys, CurvedNavigationBar for bottom navigation, pageTransitionsTheme, changed Theme, useMaterial3 to true, using getx state management, init variables, etc...

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,35 @@
-import 'package:beanstalkedu_news_api/utils/connection_manager_controller.dart';
+import 'dart:async';
+
+import 'package:beanstalkedu_news_api/Screens/Bookmarks/BookmarksScreen.dart';
+import 'package:beanstalkedu_news_api/utils/Utils.dart';
 import 'package:beanstalkedu_news_api/utils/controller_binding.dart';
+import 'package:beanstalkedu_news_api/utils/objectBoxStore/ObjectBoxStore.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 
+import 'Screens/EverythingAPI/EveryThingApiScreen.dart';
+import 'Screens/TopHeadlinesAPI/TopHeadlinesApiScreen.dart';
+
+final navigatorKey = GlobalKey<NavigatorState>(debugLabel: 'navigatorKey');
+RxInt mainNavCurrentSelectedTab = 0.obs;
+GlobalKey<CurvedNavigationBarState> _mainNavBottomNavigationKey =
+    GlobalKey(debugLabel: '_mainNavKey');
+
+/// Provides access to the ObjectBox Store throughout the app.
+late ObjectBox objectbox;
+
 Future<void> main() async {
+  // This is required so ObjectBox can get the application directory
+  // to store the database in.
+  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
+  try {
+    objectbox = await ObjectBox.create();
+  } catch (e) {
+    debugPrint('main.dart | CATCH | $e');
+  }
   runApp(const MyApp());
 }
 
@@ -16,13 +40,25 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       initialBinding: ControllerBinding(),
-      title: 'Flutter Demo',
+      navigatorKey: navigatorKey,
+      title: 'News',
       theme: ThemeData(
-          useMaterial3: true,
-          primarySwatch: Colors.blue,
-          appBarTheme: const AppBarTheme(
-              color: Colors.blue, foregroundColor: Colors.white)),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+              useMaterial3: true,
+              primarySwatch: Colors.blue,
+              appBarTheme: const AppBarTheme(
+                  color: Colors.blue, foregroundColor: Colors.white),
+              cardTheme: const CardTheme(color: Colors.white))
+          .copyWith(
+              pageTransitionsTheme: const PageTransitionsTheme(
+                  builders: <TargetPlatform, PageTransitionsBuilder>{
+            TargetPlatform.android: ZoomPageTransitionsBuilder(),
+          })),
+      initialRoute: '/',
+      getPages: [
+        GetPage(
+            name: Utils().routeConst['home']!,
+            page: () => const MyHomePage(title: 'News API')),
+      ],
     );
   }
 }
@@ -37,43 +73,43 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  RxInt counter = 0.obs;
-
-  void _incrementCounter() {
-    counter = counter++;
-  }
+  static final List<String> navTitles = [
+    'Everything API',
+    'Top headlines API',
+    'Bookmarks'
+  ];
+  static final List<Widget> _mainNavItems = <Widget>[
+    const EveryThingApiScreen(),
+    const TopHeadlinesApiScreen(),
+    const BookmarksScreen()
+  ];
 
   @override
   Widget build(BuildContext context) {
-    final ConnectionManagerController controller =
-        Get.find<ConnectionManagerController>();
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Obx(() => Text(navTitles[mainNavCurrentSelectedTab.value])),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '${dotenv.env['ESCAPED_DOLLAR_SIGN']}',
-            ),
-            Obx(() => Text(
-                  controller.connectionType.value == 1
-                      ? "Wifi Connected"
-                      : controller.connectionType.value == 2
-                          ? 'Mobile Data Connected'
-                          : 'No Internet',
-                  style: Theme.of(context).textTheme.headline4,
-                )),
-          ],
-        ),
+      bottomNavigationBar: CurvedNavigationBar(
+        key: _mainNavBottomNavigationKey,
+        height: 50,
+        index: mainNavCurrentSelectedTab.value,
+        animationCurve: Curves.slowMiddle,
+        backgroundColor: Colors.lightBlueAccent,
+        color: Colors.blueAccent,
+        buttonBackgroundColor: Colors.blueAccent,
+        items: <Widget>[
+          Utils().getCustomizedNavItem(Icons.home_outlined, 0, 'Home'),
+          Utils().getCustomizedNavItem(Icons.topic_outlined, 1, 'Top Heading'),
+          Utils()
+              .getCustomizedNavItem(Icons.bookmarks_outlined, 2, 'Book Marks'),
+        ],
+        onTap: (index) {
+          mainNavCurrentSelectedTab.value = index;
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      body: Obx(() => _mainNavItems.elementAt(mainNavCurrentSelectedTab.value)),
     );
   }
 }
