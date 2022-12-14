975186a38645a1f1d2e7cc7b282e066fad761bf5
Deven <mymartplac@gmail.com>
2022-12-14 06:12:35 +0530

    NewsApiProvider.dart | for both (getEveryThingAPI,getTopHeadingAPI) method added Uri.encodeFull to encode URL for api call and convert different params to 1 map<string,dynamic>

--- lib/services/NewsApiProvider.dart
+++ lib/services/NewsApiProvider.dart
@@ -6,19 +6,17 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class NewsApiProvider extends GetConnect {
   // Get request
-  Future<Response> getEveryThingAPI(
-      String query, int pageSize, int currentPage) {
-    String getUrl =
-        '${dotenv.env['EVERYTHING_API_URL']}?q=$query&apiKey=${dotenv.env['API_KEY']}&pageSize=$pageSize&page=$currentPage';
+  Future<Response> getEveryThingAPI(Map<String, dynamic> params) {
+    String getUrl = Uri.encodeFull(
+        '${dotenv.env['EVERYTHING_API_URL']}?apiKey=${dotenv.env['API_KEY']}&q=${params['query']}&pageSize=${params['pageSize']}&page=${params['currentPage']}');
     debugPrint('getEveryThingAPI | getUrl : $getUrl');
     return get(getUrl);
   }
 
   // Get request
-  Future<Response> getTopHeadingAPI(
-      String query, int pageSize, int currentPage) {
-    String getUrl =
-        '${dotenv.env['TOP_HEADING_API_URL']}?q=$query&apiKey=${dotenv.env['API_KEY']}&pageSize=$pageSize&page=$currentPage';
+  Future<Response> getTopHeadingAPI(Map<String, dynamic> params) {
+    String getUrl = Uri.encodeFull(
+        '${dotenv.env['TOP_HEADING_API_URL']}?apiKey=${dotenv.env['API_KEY']}&q=${params['query']}&pageSize=${params['pageSize']}&page=${params['currentPage']}');
     debugPrint('getTopHeadingAPI | getUrl : $getUrl');
     return get(getUrl);
   }
